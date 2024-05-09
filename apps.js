c26e0f7f02bb60401e28a1a7ea1b6165eb211454
sam45643 <122574805+sam45643@users.noreply.github.com>
2024-05-08 19:04:18 -0500

    Update apps.js

---
+++ apps.js
@@ -0,0 +1,6 @@
+const backArrow = document.querySelector('.back-arrow');
+
+backArrow.addEventListener('click', () => {
+  //back button website
+  window.location.href = 'https://sam45643.github.io/main-page.github.io/';
+});
